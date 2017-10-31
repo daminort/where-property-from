77609249aa84ec7b5e7256f90dda29287e97f55a
daminort <daminort@gmail.com>
2017-10-31 20:03:24 +0200

    Formatting

--- where-property-from.js
+++ where-property-from.js
@@ -5,30 +5,30 @@
 class Human {
     constructor(name) {
         this.name = name;
-	}
-	getName() {
-		return this.name;
-	}
+    }
+    getName() {
+        return this.name;
+    }
 }
 
 class Man extends Human {
     constructor(name, age) {
         super(name);
         this.age = age;
-	}
-	getAge() {
-		return this.age;
-	}
+    }
+    getAge() {
+        return this.age;
+    }
 }
 
 class Student extends Man {
     constructor(name, age, subject) {
         super(name, age);
         this.subject = subject;
-	}
-	getSubject() {
-		return this.subject;
-	}
+    }
+    getSubject() {
+        return this.subject;
+    }
 }
 
 /** ------------------------------------------------------------------------------------------------------
@@ -41,20 +41,23 @@ class Student extends Man {
  * @return {array} массив, содержащий классы: от последнего потомка до главного предка
  */
 function getClassHierarchy(obj, hierarchy) {
-    
-    hierarchy = ( hierarchy || [] );
+    hierarchy = hierarchy || [];
 
-    if (obj === null || obj.__proto__ === null || obj.__proto__.constructor === null) {
+    if (
+        obj === null ||
+        obj.__proto__ === null ||
+        obj.__proto__.constructor === null
+    ) {
         return hierarchy;
-    };
+    }
 
     hierarchy.push(obj.__proto__.constructor);
     if (obj.__proto__.constructor.prototype) {
         // уходим в рекурсию: на уровень выше
         getClassHierarchy(obj.__proto__.constructor.prototype, hierarchy);
-    };    
+    }
     return hierarchy;
-};
+}
 
 /** ------------------------------------------------------------------------------------------------------
  * Основная функция: Чье же это свойство?
@@ -66,19 +69,17 @@ function getClassHierarchy(obj, hierarchy) {
  * @return {string} строковое представление класса, в котором присутствует реализация искомого метода или свойство
  */
 function getPropertyOwner(obj, propertyName) {
-
     if (obj === null) {
         return null;
-    };
+    }
 
     // получим иерархию классов объекта
     let classHierarchy = getClassHierarchy(obj);
 
     // проверяем у самого объекта
     let props = Object.getOwnPropertyNames(obj);
-    if ( props.indexOf(propertyName) !== -1 ) {
+    if (props.indexOf(propertyName) !== -1) {
         return obj.constructor.name;
-
     } else {
         // сначала получим класс объекта
         let objClass = null;
@@ -86,21 +87,21 @@ function getPropertyOwner(obj, propertyName) {
             objClass = classHierarchy[i];
             if (obj instanceof objClass) {
                 break;
-            };
-        };    
+            }
+        }
         if (objClass === null) {
             return 'Object';
         }
         // теперь проверяем наличие необходимого свойства
         let objProps = Object.getOwnPropertyNames(objClass);
-        if ( objProps.indexOf(propertyName) !== -1 ) {
+        if (objProps.indexOf(propertyName) !== -1) {
             return objClass.constructor.name;
         } else {
             // уходим в рекурсию: на уровень выше
             return getPropertyOwner(objClass.prototype, propertyName);
-        };
-    };
-};
+        }
+    }
+}
 /** ------------------------------------------------------------------------------------------------------
  * Основная программа
  ------------------------------------------------------------------------------------------------------ */
@@ -116,13 +117,13 @@ let getAgeOwner = getPropertyOwner(john, 'getAge');
 let getNameOwner = getPropertyOwner(john, 'getName');
 let toStringOwner = getPropertyOwner(john, 'toString');
 
-console.dir( john );
-console.log( johnName );
-console.log( johnAge );
-console.log( johnSubject );
-console.log( '----' );
-console.log( 'Владелец свойства age: ' + ageOwner );                // Student
-console.log( 'Владелец метода getSubject: ' + getSubjectOwner );    // Student
-console.log( 'Владелец метода getAge: ' + getAgeOwner );            // Man
-console.log( 'Владелец метода getName: ' + getNameOwner );          // Human
-console.log( 'Владелец метода toString: ' + toStringOwner );        // Object
+console.dir(john);
+console.log(johnName);
+console.log(johnAge);
+console.log(johnSubject);
+console.log('----');
+console.log('Владелец свойства age: ' + ageOwner); // Student
+console.log('Владелец метода getSubject: ' + getSubjectOwner); // Student
+console.log('Владелец метода getAge: ' + getAgeOwner); // Man
+console.log('Владелец метода getName: ' + getNameOwner); // Human
+console.log('Владелец метода toString: ' + toStringOwner); // Object
